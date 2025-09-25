f20973f4b985e5e449416faa28ac7c7289a83256
02Maksym016 bc2025-2 <maximfadeev5555@gmail.com>
2025-09-25 20:14:00 +0300

    Додаю main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-consol.log('Hello world')
+// Імпортуємо клас URL та URLSearchParams
+const { URL, URLSearchParams } = require('url');
+
+// Константи для параметрів (варіант 6)
+const date = "20231001";     // 1 жовтня 2023 у форматі YYYYMMDD
+const time_period = "q";     // q = квартал
+
+// Створюємо базовий URL НБУ
+const myUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/bankact");
+
+// Додаємо параметри запиту
+myUrl.search = new URLSearchParams({
+    date: date,
+    time_period: time_period,
+    json: "" // щоб отримати відповідь у форматі JSON
+});
+
+// Виводимо готовий URL у консоль
+console.log(myUrl.toString());
